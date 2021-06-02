06c15b11952ceb003ee20368134182817192c2d5
narenkv <narenkv@gmail.com>
2021-06-02 17:22:00 +0530

    Update sayHello2.groovy

--- vars/sayHello2.groovy
+++ vars/sayHello2.groovy
@@ -4,5 +4,14 @@ import hudson.model.User
 import hudson.security.Permission
 
 def call(String name = 'human') {
-  echo "Hello, ${name}."
+  
+  def inst = Jenkins.getInstanceOrNull()
+  def strategy = inst.getAuthorizationStrategy()
+  strategy.getMetaPropertyValues().find { it.getName() == 'grantedPermissions'}.each {
+      // it.value is a map with permission as key and the corresponding users as value
+      print(it.value[Permission.READ])
+      print(it.value[Permission.WRITE])
+      print(it.value[Permission.HUDSON_ADMINISTER])
+  }
+  
 }
