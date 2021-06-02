be27d7e3dc1937b8b9eddf8c67e29ce9bf5eabc0
narenkv <narenkv@gmail.com>
2021-06-03 00:24:29 +0530

    Update sayHello2.groovy

--- vars/sayHello2.groovy
+++ vars/sayHello2.groovy
@@ -52,9 +52,8 @@ def call(String name = 'human') {
    Map<hudson.security.Permission,Set<String>>  gp = autToUpdate.getGrantedPermissions();
    println(gp);
   
-   gp.get(hudson.security.Permission.fromId("hudson.plugins.promoted_builds.Promotion")).each{ us ->
-         User usr = User.getById(us,false);
-         println(us);
+   gp.get(hudson.security.Permission.fromId("hudson.model.Item.Configure")).each{ us ->
+          println(us);
      
    }
   //GlobalMatrixAuthorizationStrategy auth = new GlobalMatrixAuthorizationStrategy();
