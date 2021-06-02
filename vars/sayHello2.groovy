aec141c1bbd72648f4ee8fb6a42a2d620f851755
narenkv <narenkv@gmail.com>
2021-06-02 17:23:15 +0530

    Update sayHello2.groovy

--- vars/sayHello2.groovy
+++ vars/sayHello2.groovy
@@ -3,7 +3,7 @@
 import hudson.model.User
 import hudson.security.Permission
 
-def call(String name = 'human') {
+def call() {
   
   def inst = Jenkins.getInstanceOrNull()
   def strategy = inst.getAuthorizationStrategy()
